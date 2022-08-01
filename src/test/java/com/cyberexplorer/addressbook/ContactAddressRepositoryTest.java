1d80342ff37b1a38e236f3cb54aa408e25568c4c
Xinyi Zhou <cintheswagster2015@gmail.com>
2022-08-01 00:09:13 -0500

    AB-11 Add Unit Test Case for finding Contact Address by Name and JUnit Test 4 Configuration.

---
+++ src/test/java/com/cyberexplorer/addressbook/ContactAddressRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.cyberexplorer.addressbook;
+
+import com.cyberexplorer.addressbook.model.BusinessContactAddress;
+import com.cyberexplorer.addressbook.model.ContactAddress;
+import com.cyberexplorer.addressbook.model.ContactAddressRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest(properties = "spring.profiles.active:test")
+public class ContactAddressRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ContactAddressRepository contactAddressRepository;
+
+    @Test
+    public void testFindByName() {
+        entityManager.persist(new BusinessContactAddress("James Chou", "636-172-7246", "james_chou@abc.com", "BUSINESS", true, "www.Sharkbeyond.com"));
+        ContactAddress contactAddress = contactAddressRepository.findByName("James Chou");
+        assertEquals("James Chou", contactAddress.getName());
+    }
+}
+
